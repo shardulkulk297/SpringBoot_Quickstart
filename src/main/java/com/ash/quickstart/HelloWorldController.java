abe6edc2c33924eb99b91bb45f5b1e6e77ea55b3
Shardul <shardulkulk297@gmail.com>
2025-05-09 23:30:40 +0530

    Streak

--- src/main/java/com/ash/quickstart/HelloWorldController.java
+++ src/main/java/com/ash/quickstart/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-
+//The Get route mapped, annotation is used to map the route to the controller method.
     @GetMapping(path = "/hello")
     public String helloWorld(){
         return "Hello World Pokemon";
